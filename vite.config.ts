060f748c7a26fed4112e5a289978d8ba888e513a
Mohammed Anas <mohammedanas1571@gmail.com>
2024-08-06 10:51:59 +0530

    vercel config is changed

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import path from "path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
@@ -10,16 +10,16 @@ export default defineConfig({
     },
   },
   define: {
-    global: {}},
+    global: {}
+  },
   server: {
     host: '0.0.0.0',
     proxy: {
       '/auth': {  
-        target: 'http://localhost:3000',
+        target: 'https://aflozz.shop',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/auth/, ''),
-
       }
     }
   }
-})
+});
