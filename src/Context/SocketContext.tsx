5c87bd939bc58fcb6551d7eb5df23ec5ed2808c5
Mohammed Anas <mohammedanas1571@gmail.com>
2024-08-06 10:35:25 +0530

    changed the backend url

--- src/Context/SocketContext.tsx
+++ src/Context/SocketContext.tsx
@@ -13,7 +13,7 @@ const SocketProvider = ({ children }: { children: React.ReactNode }) => {
   const { user, tutor} = useSelector((state: RootState) => state)
 
   useEffect(() => {
-    const socket = io("http://localhost:3000", { withCredentials: true });
+    const socket = io("https://aflozz.shop", { withCredentials: true });
     setSocket(socket);
 
     return () => {
